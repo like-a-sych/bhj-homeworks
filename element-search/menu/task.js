3ac37fc267c655d5409d93113e2967573cf13b45
John Doe <montekke@gmail.com>
2023-05-01 11:28:08 +0300

    fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,14 @@
 const elemsMenu = document.querySelectorAll('.menu__link');
 
-document.addEventListener('click', function (target) {
-	let link = target.target;
+document.addEventListener('click', function (event) {
+	let link = event.target;
 	if (link.classList.contains('menu__link') && link.parentElement.querySelector('.menu.menu_sub')) {
-		target.preventDefault();
-		document.querySelectorAll('.menu.menu_sub').forEach((item) => { item.classList.remove('menu_active'); })
-		link.parentElement.querySelector('.menu.menu_sub').classList.add('menu_active');
+		event.preventDefault();
+		if (link.parentElement.querySelector('.menu.menu_sub').classList.contains('menu_active')) {
+			document.querySelectorAll('.menu.menu_sub').forEach((item) => { item.classList.remove('menu_active'); })
+		} else {
+			link.parentElement.querySelector('.menu.menu_sub').classList.add('menu_active');
+		}
 	} else {
 		document.querySelectorAll('.menu.menu_sub').forEach((item) => { item.classList.remove('menu_active'); })
 	}
