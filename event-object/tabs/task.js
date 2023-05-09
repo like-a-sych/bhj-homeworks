764b13cc9d4576aa6db36ad3f2df57fbd66aed26
John Doe <montekke@gmail.com>
2023-05-09 10:56:25 +0300

    fix

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -4,14 +4,15 @@ function navigation(id) {
 	const navs = navTabs.querySelectorAll('.tab');
 	const tabs = navTabs.querySelectorAll('.tab__content');
 
-	navTabs.addEventListener('click', () => {
-		if (!(event.target.classList.contains('tab_active'))) {
-			let index = Array.from(navs).indexOf(event.target);
+	navs.forEach(element => {
+		element.addEventListener('click', (event) => {
 			navTabs.querySelector('.tab.tab_active').classList.remove('tab_active');
-			navTabs.querySelector('.tab__content.tab__content_active').classList.remove('tab__content_active');
-			event.target.classList.add('tab_active')
-			tabs[index].classList.add('tab__content_active')
-		}
+			event.target.classList.add("tab_active");
+			tabs.forEach(element => {
+				element.classList.remove('tab__content_active');
+			})
+			tabs[Array.from(navs).indexOf(event.target)].classList.add('tab__content_active');
+		})
 	})
 }
 
